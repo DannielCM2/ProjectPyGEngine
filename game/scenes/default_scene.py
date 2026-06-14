cb0b3eed52915801543257d390b2544d9219972b
DannielCM2 <danielcanaria9@gmail.com>
2026-06-15 01:48:22 +0800

    Removed test level and replaced it with default_scene

--- game/scenes/default_scene.py
+++ game/scenes/default_scene.py
@@ -1,17 +1,18 @@
-from game.game_objects.cube_3d import Cube3D
-from engine.scene import Scene
+import game
 import glfw
 
-class Level1(Scene):
+from engine import Scene
+
+class DefaultScene(Scene):
     def __init__(self, engine):
         super().__init__(engine=engine)
         self.on_load()
 
     def on_load(self):
-        self.objects.append(Cube3D(self.engine.window.ctx))
-        print(self.objects)
+        self.objects.append(game.Cube3D(self.engine.window.ctx))
 
     def update(self, dt):
         if self.input.was_pressed(glfw.KEY_ESCAPE):
             print("Loading level2")
+            return
             self.scene_manager.load_scene('level2')
