a9e2a2215389741e848123d4255c8f5a34204f48
DannielCM2 <danielcanaria9@gmail.com>
2026-06-15 01:48:04 +0800

    removed test data.

--- game/PyGEngine.py
+++ game/PyGEngine.py
@@ -1,13 +1,11 @@
 from engine.game import Game
-from game.scenes.level1 import Level1
-from game.scenes.level2 import Level2
+from game import scenes
 
 def start_game():
     game = Game()
 
-    game.scene_manager.cache_scene('level1', Level1)
-    game.scene_manager.cache_scene('level2', Level2)
+    game.scene_manager.cache_scene('default', scenes.DefaultScene)
 
-    game.scene_manager.load_scene("level1")
+    game.scene_manager.load_scene("default")
 
     game.run()
